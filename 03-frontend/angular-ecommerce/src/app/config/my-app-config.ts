81d151d78bcb8476b28fb2195467f50407258d07
Twisted Fate <shuliu7943@gmail.com>
2022-03-20 11:20:38 -0500

    first commit

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa47y0wi1rWoSFQc5d7',
+        issuer: 'https://dev-04425507.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
